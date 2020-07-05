a0a3e6a769ec46cae244a7f8b17db4cb7eb5c4c9
Luigi Seminara <seminara.luigi@gmail.com>
2020-07-05 17:43:04 +0200

    fix: IPv6 oneway communication

--- src/udp_oneway_ipv6/client/oneway-udp-client.c
+++ src/udp_oneway_ipv6/client/oneway-udp-client.c
@@ -1,41 +1,39 @@
-/*
-@Le triglie fritte
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#define BUFFER_SIZE 1000
 
-int main(int argc, char**argv)
-{ 
-  struct sockaddr_in6 addr;
-  char sendline[BUFFER_SIZE];
-  socklen_t len = sizeof(struct sockaddr_in6);
-  int socketd;
+int main(int argc, char** argv) {
 
-  if (argc != 3)
-  { 
-    printf("Please enter the following arguments: <Target's IPv6> <Target's port>\n");
-    return -1;
-  }
+    int sockfd;
+    struct sockaddr_in6 dest_addr;
+    extern int errno;
+    char buffer[100];
+    int n;
 
-  if((socketd=socket(PF_INET6,SOCK_DGRAM,0)) <0)
-  { 
-    printf("\nUnable to create socket, terminating...");
-    return -1;
-  }
-  memset(&addr,0,len);
-  addr.sin6_family = AF_INET6;
-  inet_pton(AF_INET6, argv[1], &(addr.sin6_addr)); 
-  addr.sin6_port=htons(atoi(argv[2]));
-   
-  while (fgets(sendline, BUFFER_SIZE,stdin) != NULL)
-  { 
-    printf("Sending '%s' to '%s'", sendline, argv[1]);
-    sendto(socketd,sendline,strlen(sendline),0, (struct sockaddr *)&addr, len);
-  }
-  return 0;
+    if(argc < 3) {
+        printf("Usage: client6 IPv6_Dest Port_Dest\n");
+        return -1;
+    }
+
+    sockfd = socket(PF_INET6, SOCK_DGRAM, 0);
+
+    if(sockfd < 0) {
+        printf("Error in socket opening ... exit!\n");
+        return -1;
+    }
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin6_family = AF_INET6;
+    inet_pton(AF_INET6, argv[1], &(dest_addr.sin6_addr));
+    dest_addr.sin6_port = htons(atoi(argv[2]));
+
+    printf("Insert an integer:\n");
+    scanf("%d", &n);
+
+    for(int i=0; i<10; i++) {
+        sprintf(buffer, "%d", i+n);
+        printf("sending %s\n", buffer);
+        sendto(sockfd, buffer, strlen(buffer)+1, 0, (struct sockaddr*) &dest_addr, sizeof(dest_addr));
+    }
 }
--- src/udp_oneway_ipv6/server/oneway-udp-server.c
+++ src/udp_oneway_ipv6/server/oneway-udp-server.c
@@ -1,47 +1,40 @@
-/*
-@Le triglie fritte
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#define BUFFER_SIZE 1000
+#include <errno.h>
 
-int main(int argc, char**argv)
-{ 
-  struct sockaddr_in6 local, remote;
-  char recvline[BUFFER_SIZE];
-  char ipv6_presentation[INET6_ADDRSTRLEN];
-  socklen_t len = sizeof(struct sockaddr_in6);
-  int socketd;
+int main(int argc, char** argv) {
+    
+    char str[INET6_ADDRSTRLEN];
+    int sockfd;
+    struct sockaddr_in6 local_addr, remote_addr;
+    socklen_t len = sizeof(struct sockaddr_in6);
+    char buffer[256];
 
-  if (argc != 2)
-  { 
-    printf("Please enter the following arguments: <Listening port>\n");
-    return -1;
-  }
+    if(argc < 2) {
+        printf("Usage: server6 PORT\n");
+        return 1;
+    }
 
-  if((socketd=socket(PF_INET6,SOCK_DGRAM,0)) <0)
-  { 
-    printf("\nUnable to create socket, terminating...");
-    return -1;
-  }
-  memset(&local,0,len);
-  local.sin6_family = AF_INET6;
-  local.sin6_port=htons(atoi(argv[1]));
+    sockfd = socket(PF_INET6, SOCK_DGRAM, 0);
+    if(sockfd < 0) {
+        printf("Error in socket opening ... exit!");
+        return -1;
+    }
 
-  if(bind(socketd, (struct sockaddr *) &local, len)<0)
-  { printf("\nBinding error!");
-    return -1;
-  }
-   
-  while (1)
-  { 
-    recvfrom(socketd,recvline,strlen(recvline),0, (struct sockaddr *)&remote, &len);
-    inet_ntop(AF_INET6, &remote, ipv6_presentation, sizeof(ipv6_presentation));
-    printf("Received from '%s' : '%s'", ipv6_presentation, recvline);
-  }
-  return 0;
+    memset((char *) &local_addr, 0, len);
+    local_addr.sin6_family = AF_INET6;
+    local_addr.sin6_port = htons(atoi(argv[1]));
+
+    if(bind(sockfd, (struct sockaddr *) &local_addr, sizeof(local_addr)) < 0) {
+        printf("Binding error! Error code n. %d \n", errno);
+        return -1;
+    }
+
+    for(;;) {
+        recvfrom(sockfd, buffer, 99, 0, (struct sockaddr *) &remote_addr, &len);
+        inet_ntop(PF_INET6, (struct in6_addr *) &remote_addr.sin6_addr, str, INET6_ADDRSTRLEN);
+        printf("Packet from IP:%s, Port:%d, msg:%s \n", str,ntohs(remote_addr.sin6_port), buffer);
+    }
 }
